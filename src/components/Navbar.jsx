5dbef398d0715b5dab763b4f163d11a2e69ece8e
dhruv <dhruv.sathe11@gmail.com>
2023-09-13 23:13:10 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,11 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-           
+          <img
+            src={logo}
+            alt=""
+
+          />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Dhruv&nbsp;<span className="sm:block hidden">| Developer</span>
           </p>

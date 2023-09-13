5847459679d5e97269ec8fe513d4508035725068
dhruv <dhruv.sathe11@gmail.com>
2023-09-13 22:13:55 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
           }}
         >
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Dhruv&nbsp;<span className="sm:block hidden">| Developer</span>
+            Find &nbsp;<span className="sm:block hidden"> me here&nbsp; &rarr;</span>
           </p>
         </Link>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">

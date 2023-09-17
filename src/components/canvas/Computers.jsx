605c21427e536b1a8e7b964e5f551f6d29191a52
dhruv <dhruv.sathe11@gmail.com>
2023-09-17 13:59:34 +0530

    ok

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -59,12 +59,13 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 5], fov: 25, near: 0.1, far: 200 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       {/* Canvas Loader show on fallback */}
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
